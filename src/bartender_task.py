17f3e56a65ea6608ae2859a6a4a858585a1d8caa
alan-sanchez <44442727+alan-sanchez@users.noreply.github.com>
2024-07-14 17:13:11 -0700

    Removed assistant prompt

--- src/bartender_task.py
+++ src/bartender_task.py
@@ -15,6 +15,7 @@ from pathlib import Path
 from openai import OpenAI
 from std_msgs.msg import String
 from halo import Halo
+from geometry_msgs.msg import Pose
 
 class BarTask():
     """
@@ -25,16 +26,20 @@ class BarTask():
         Parameters:
         - self: The self reference. 
         """
-        # self.pub = rospy.Publisher('/talk', String, queue_size=10)
-        self.sub = rospy.Subscriber('object_map', String, self.callback, queue_size=10)
+        ## Initialize subscriber
+        self.object_map_sub = rospy.Subscriber('object_map', String, self.callback, queue_size=10)
+        
+        ## Initialize publisher
+        self.waypoint_pub = rospy.Publisher('waypoint', Pose, queue_size=1)
         
         ## Specify the relative and audio directory paths
         self.relative_path = 'catkin_ws/src/voice_command_interface/'
         self.system_filename = 'system_prompt.txt'
-        self.assistant_filename = 'assistant_prompt.txt'
-        self.assistant_dir = os.path.join(os.environ['HOME'], self.relative_path, 'prompts', self.assistant_filename)
-        # self.intro_audio_dir = os.path.join(os.environ['HOME'], self.relative_path, 'audio_files/intro.wav')
+        self.system_filename_dir = os.path.join(os.environ['HOME'], self.relative_path, 'prompts', self.system_filename)
+      
+        ##
         self.audio_filename = 'intro.wav'
+
         ## Get the OpenAI API key from environment variables
         self.key = os.environ.get("openai_key")
         
@@ -50,22 +55,25 @@ class BarTask():
         self.fs = 44100  # Sampling rate in Hz
         self.default_time = 10
 
+        ## hard code line number
+        self.line_number = 10
+
         ##
         self.spinner = Halo(text='Computing response', spinner='dots')
 
         ## Log initialization notifier
         rospy.loginfo('{}: is ready.'.format(self.__class__.__name__))
-
-        ## hard code line number
-        self.line_number = 9
-
+        
 
     def callback(self, msg):
+        '''
+        
+        '''
         self.object_map_dict = json.loads(msg.data)
         self.label_list = list(self.object_map_dict.keys())
 
         # Read the file contents
-        with open(self.assistant_dir, 'r') as file:
+        with open(self.system_filename_dir, 'r') as file:
             lines = file.readlines()
 
         # Delete the specified line
@@ -73,7 +81,7 @@ class BarTask():
         lines.insert(self.line_number - 1, str(self.label_list) + '\n')
 
         # Write the modified contents back to the file
-        with open(self.assistant_dir, 'w') as file:
+        with open(self.system_filename_dir, 'w') as file:
             file.writelines(lines)
 
 
@@ -97,22 +105,26 @@ class BarTask():
         os.remove(temp_filename)
 
         ## 
-        response = self.ttt.text_to_text(system_filename=self.system_filename, assistant_filename=self.assistant_filename, user_prompt=transcript)
+        response = self.ttt.text_to_text(system_filename=self.system_filename, user_prompt=transcript)
         
-
         ## 
         return ast.literal_eval(response)
     
     
     def get_task(self, dict_response):
-        ## Assuming the dictionary is len 1
+        '''
+        
+        '''
+        ## Assuming the dictionary length size is 1
         key_list = list(dict_response.keys())
         value_list = list(dict_response.values())
-        value_list = value_list[0]
+       
+        ## Greeting condition
         if key_list[0] == 'A':
             self.spinner.stop()
             self.tts.playback(self.audio_filename)
-            
+
+        ## Cocktail list condition 
         elif key_list[0] == 'B':
             text = "With the options on the table, you can make a "
 
@@ -125,21 +137,33 @@ class BarTask():
                         text = text + str(e) + ', '
                     else:
                         text = text + "and " + str(e) + '.'
-                    
             
+            ## 
             drink_list_filename = "drink_list.wav"
             self.tts.convert_to_speech(text=text, filename=drink_list_filename)
             self.spinner.stop()
             print(text)
             self.tts.playback(drink_list_filename)
 
-
+        ## Manipulation condition
         elif key_list[0] == 'C':
-            print(key_list[0])
+            ##
+            coordinates = self.object_map_dict.get(value_list[0])
+            
+            ## Create a Pose message type
+            p = Pose()
+            p.position.x = coordinates[0]
+            p.position.y = coordinates[1]
+            p.position.z = coordinates[2]
+            p.orientation.x = 0.0
+            p.orientation.y = 0.0
+            p.orientation.z = 0.0
+            p.orientation.w = 1.0
 
-        
-
-
+            self.waypoint_pub.publish(p)
+            
+            self.spinner.stop()
+            print("waypoint sent")
             
 
 if __name__ == '__main__':
