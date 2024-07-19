705eff310920fda445ed27437921e5641dbc5bbd
Alan G. Sanchez <sanchala@oregonstate.edu>
2024-07-19 14:06:56 -0700

    Included task_executive class

--- src/task_1_executive.py
+++ src/task_1_executive.py
@@ -7,180 +7,276 @@ import os
 import signal
 import tf
 import json
+import ast
 import actionlib
 import moveit_commander 
 import moveit_msgs.msg
 
 ## Import message types and other python libraries
-from moveit_python import PlanningSceneInterface, MoveGroupInterface
+from moveit_python import MoveGroupInterface, PlanningSceneInterface
 from moveit_msgs.msg import MoveItErrorCodes
-from geometry_msgs.msg import  Pose, Point, Quaternion
+from geometry_msgs.msg import  Pose, Point, Quaternion, PoseArray, PoseStamped
+from std_msgs.msg import String, Header
+from visualization_msgs.msg import Marker, MarkerArray
 from robot_controllers_msgs.msg import QueryControllerStatesAction, QueryControllerStatesGoal, ControllerState
 from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
-
-
-# Define a class attribute or a global variable as a flag
-interrupted = False
-
-# Define a signal handler function
-def signal_handler(signal, frame):
-    global interrupted
-    interrupted = True
-
-# Register the signal handler
-signal.signal(signal.SIGINT, signal_handler)
+  
 
 class MoveGroupClient:
     '''
-    Class for controlling mid-level operations of the arm.
+    Class for controlling mid-level operations of the arm and torso.
     '''
     def __init__(self):
         '''
-        A function that ...
-        :param self: The self reference.
+        A function that initializes the MoveGroupClient.
         '''
+        ## Notify user for Moveit connection
         rospy.loginfo("Waiting for MoveIt!")
-        self.client = MoveGroupInterface("arm_with_torso", "base_link")
 
-        ## First initialize `moveit_commander`
-        moveit_commander.roscpp_initialize(sys.argv)
+        self.move_group = MoveGroupInterface("arm_with_torso", "base_link")
+        self.gripper_frame = ("gripper_link")
 
-        ## Initialize publisher
-        self.waypoint_sub = rospy.Subscriber('waypoint', Pose, self.callback)
+        # ## Padding does not work (especially for self collisions), so we are adding a box above the base of the robot
+        self.planning_scene = PlanningSceneInterface("base_link")
+        self.planning_scene.addBox("keepout", 0.25, 0.5, 0.09, 0.15, 0.0, 0.375)
+        self.planning_scene.addBox("table", 0.6, 1.5, 0.05, 0.69, 0.0, 0.75)
 
-        ## Instantiate a `RobotCommander`_ object. This object is the outer-level
-        ## interface to the robot
-        self.robot = moveit_commander.RobotCommander()
-
-        ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
-        ## to one group of joints.
-        self.group = moveit_commander.MoveGroupCommander("arm_with_torso")
-        self.group.set_end_effector_link("gripper_link") #use gripper_link if it is planar disinfection
-
-        ## We create a `DisplayTrajectory`_ publisher which is used later to publish
-        ## trajectories for RViz to visualize:
-        self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-                                                       moveit_msgs.msg.DisplayTrajectory,
-                                                       queue_size=20)
-        rospy.loginfo("...connected")
-
-        ## Padding does not work (especially for self collisions)
-        ## So we are adding a box above the base of the robot
-        self.scene = PlanningSceneInterface("base_link")
-        self.scene.addBox("keepout", 0.25, 0.5, 0.09, 0.15, 0.0, 0.375)
-
-        ## Allow replanning to increase the odds of a solution
-        self.group.allow_replanning(True)
-
-        ## Set the maximum velocity and acceleration scaling factors
-        self.group.set_max_velocity_scaling_factor(0.1)  # 10% of the maximum speed
-        self.group.set_max_acceleration_scaling_factor(0.5)  # 10% of the maximum acceleration
-
-        ## Create the list of joints
-        self.joints = ["torso_lift_joint", "shoulder_pan_joint", "shoulder_lift_joint", "upperarm_roll_joint",
-                  "elbow_flex_joint", "forearm_roll_joint", "wrist_flex_joint", "wrist_roll_joint"]
-                
-    def callback(self, pose):
-        # raw_input("Press enter to move pose")
-        state = self.move_to_pose(pose)
-        rospy.sleep(2)
-        self.init_pose()
-
-    def init_pose(self, vel = 0.2):
+      
+    def init_pose(self, vel=0.2):
         """
-        Function that sends a joint goal that moves the Fetch's arm and torso to
-        the initial position.
-        :param self: The self reference.
-        :param vel: Float value for arm velocity.
+        Function that sends a joint goal that moves the Fetch's arm and torso to the initial position.
         """
-        ## List of joint values of init position
-        pose =[.27, 1.41, 0.30, -0.22, -2.25, -1.56, 1.80, -0.37,]
+        ##
+        joints = ["torso_lift_joint", "shoulder_pan_joint", "shoulder_lift_joint", "upperarm_roll_joint",
+                  "elbow_flex_joint", "forearm_roll_joint", "wrist_flex_joint",    "wrist_roll_joint"]
+
+        ## List of joint values for initial position
+        pose = [.27, 1.41, 0.30, -0.22, -2.25, -1.56, 1.80, -0.37]
+
         while not rospy.is_shutdown():
-            result = self.client.moveToJointPosition(self.joints,
-                                                     pose,
-                                                     0.0,
-                                                     wait=True,
-                                                     max_velocity_scaling_factor=vel)
+            result = self.move_group.moveToJointPosition(joints,
+                                                         pose,
+                                                         0.0,
+                                                         max_velocity_scaling_factor=vel)
             if result and result.error_code.val == MoveItErrorCodes.SUCCESS:
-                self.scene.removeCollisionObject("keepout")
+                self.planning_scene.removeCollisionObject("keepout")
                 return 0 
+ 
 
-    def move_to_pose(self, pose):
-        # Set the target pose
-        self.group.set_pose_target(pose)
 
-        # Plan the motion to the target pose
-        plan = self.group.plan()
-
-        # print(plan)
-        # Execute the planned motion
-        success = self.group.go(wait=True)
-
-        # # Stop any residual movement
-        # self.group.stop()
-
-        # # Clear the pose target
-        # self.group.clear_pose_targets()
-
-        return success
-
-    
-class PointHeadClient(object):
+class PointHeadClient:
+    '''
+    Class for controlling the head movements.
+    '''
     def __init__(self):
-        self.client = actionlib.SimpleActionClient("head_controller/follow_joint_trajectory", FollowJointTrajectoryAction)
+        '''
+        Initialize the PoinHeadClient.
+        '''
+        self.head_client = actionlib.SimpleActionClient("head_controller/follow_joint_trajectory", FollowJointTrajectoryAction)
         rospy.loginfo("Waiting for head_controller...")
-        self.client.wait_for_server()
+        self.head_client.wait_for_server()
         rospy.loginfo("...connected")
-        
 
     def set_pan_tilt(self, pan=0.00, tilt=0.86, duration=1.0):
-        # Create a trajectory point
+        '''
+        Set the pan and tilt for the head.
+
+        Parameters:
+        pan ():
+        tilte ():
+        duration()
+
+        '''
+        ## Create a trajectory point
         point = JointTrajectoryPoint()
         point.positions = [pan, tilt]
         point.time_from_start = rospy.Duration(duration)
 
-        # Create a trajectory
+        ## Create a trajectory
         trajectory = JointTrajectory()
         trajectory.joint_names = ["head_pan_joint", "head_tilt_joint"]
         trajectory.points = [point]
 
-        # Create a goal
+        ## Create a goal
         goal = FollowJointTrajectoryGoal()
         goal.trajectory = trajectory
 
-        # Send the goal to the action server
-        self.client.send_goal(goal)
-        self.client.wait_for_result()
+        ## Send the goal to the action server
+        self.head_client.send_goal(goal)
+        self.head_client.wait_for_result()
 
+
+
+class TaskExecutive(MoveGroupClient, PointHeadClient):
+    '''
+    Class for task execution combining arm and head control.
+    '''
+    def __init__(self):
+        ''' 
+        
+        '''
+        MoveGroupClient.__init__(self)
+        PointHeadClient.__init__(self)
+
+        #
+        self.init_pose()
+        self.set_pan_tilt()
+
+        ## Initialize subsciber
+        self.contaminated_obj_sub = rospy.Subscriber('contaminated_objects',String, self.waypoint_generator)
+
+        self.complete_pub = rospy.Publisher('cleaning_status', String, queue_size=10)
+
+        ##
+        # self.waypoints_marker_pub = rospy.Publisher('waypoints_marker', Marker    , queue_size=1)
+        self.waypoints_marker_pub = rospy.Publisher('waypoints', MarkerArray, queue_size=1)
+        # Setup header
+        self.header = Header()
+        self.header.frame_id = "/base_link"
+        self.header.stamp = rospy.Time.now()
+    
+        # Initialize waypoint_markers and all of the other feature values
+        self.waypoints_marker = Marker()
+        self.waypoints_marker.header = self.header
+        self.waypoints_marker.type = Marker.ARROW
+        self.waypoints_marker.scale.x = 0.03
+        self.waypoints_marker.scale.y = 0.01
+        self.waypoints_marker.scale.z = 0.005
+        self.waypoints_marker.color.a = 1
+        self.waypoints_marker.color.r = 0
+        self.waypoints_marker.color.g = 0
+        self.waypoints_marker.color.b = 1.0
+
+        ## Initialize dictionary
+        self.contaminated_obj_dict = {}
+
+        ## Initialize table height where the objects were tested on
+        self.test_table_height = 0.77
+
+        ## Specify the relative and audio directory paths
+        self.relative_path = 'catkin_ws/src/voice_command_interface/tool_paths'        
+
+        ## Log initialization notifier
+        rospy.loginfo('{}: is ready.'.format(self.__class__.__name__))
+
+
+
+    def waypoint_generator(self,msg):
+        '''
+
+        '''
+        ##
+        contaminated_obj_dict = ast.literal_eval(msg.data)
+        # Reverse the dictionary
+        # reversed_dict = {value: key for key, value in contaminated_obj_dict.items()}
+        # print("Original Dictionary:")
+        print(contaminated_obj_dict)
+        # print()
+
+        # Sorting the dictionary by the centroid values in descending order
+        # sorted_data = dict(sorted(contaminated_obj_dict.keys(), key=lambda item: item['centroid'], reverse=True))
+
+
+
+        # # Reconstruct the dictionary from the sorted items
+        # # sorted_dict = dict(sorted_items)
+
+        # print("Sorted Dictionary:")
+        # print(sorted_items)
+
+        waypoints = []
+        marker_array = MarkerArray()
+
+        for key, value in contaminated_obj_dict.items():
+            file_path = os.path.join(os.environ['HOME'],self.relative_path, key + '.json')
+
+            if os.path.exists(file_path):
+                with open(file_path) as user_file:
+                    poses = json.load(user_file)
+                    # print(len(poses))
+
+                # Calculate shifts required to align the pre-recorded path with the current object position
+                shift_x = round(value['centroid'][0] - poses[0][0], 2)
+                shift_y = round(value['centroid'][1] - poses[0][1], 2)
+                shift_z = round(value['table_height'] - self.test_table_height, 2)
+
+                for i, pose in enumerate(poses):
+                    new_pose = Pose(Point(pose[0] + shift_x, pose[1] + shift_y, pose[2] + shift_z),
+                                          Quaternion(pose[3], pose[4], pose[5], pose[6])
+                                    )
+                    waypoints.append(new_pose)
+
+                    # Create and append a marker for visualization
+                    marker = Marker()
+                    marker.header.frame_id = "base_link"
+                    marker.header.stamp = rospy.Time.now()
+                    marker.ns = "waypoints"
+                    marker.id = i
+                    marker.type = Marker.SPHERE
+                    marker.action = Marker.ADD
+                    marker.pose = new_pose
+                    marker.scale.x = 0.01
+                    marker.scale.y = 0.01
+                    marker.scale.z = 0.01
+                    marker.color.a = 1.0
+                    marker.color.r = 1.0
+                    marker.color.g = 0.0
+                    marker.color.b = 0.0
+                    marker_array.markers.append(marker)
+
+
+        self.waypoints_marker_pub.publish(marker_array)
+        # print(shift_z)
+        # rospy.loginfo("Published waypoints markers")
+        self.move_along_waypoints(waypoints)
+
+
+    def move_along_waypoints(self, waypoints):
+
+        gripper_pose_stamped = PoseStamped()
+        gripper_pose_stamped.header.frame_id = "base_link"
+        for waypoint in waypoints:    
+            ##
+            gripper_pose_stamped.header.stamp = rospy.Time.now()
+
+            ## 
+            gripper_pose_stamped.pose = waypoint
+
+            self.move_group.moveToPose(pose_stamped=gripper_pose_stamped,
+                                       gripper_frame=self.gripper_frame,
+                                       tolerance=0.1,
+                                       max_velocity_scaling_factor=0.2,)
+
+            result = self.move_group.get_move_action().get_result()
+
+            if result:
+                # Checking the MoveItErrorCode
+                if result.error_code.val == MoveItErrorCodes.SUCCESS:
+                    # rospy.loginfo("Made it to waypoint")
+                    flag = True
+                else:
+                    # If you get to this point please search for:
+                    # moveit_msgs/MoveItErrorCodes.msg
+                    rospy.logerr("Arm goal in state: %s",
+                                 self.move_group.get_move_action().get_state())
+            else:
+                rospy.logerr("MoveIt! failure no result returned.")
+
+        # This stops all arm movement goals
+        # It should be called when a program is exiting so movement stops
+        # self.move_group.get_move_action().cancel_all_goals()
+
+        
+        self.init_pose()
+        
 
 
 if __name__ == '__main__':
     ## Initialize the `relax_arm_control` node
-    rospy.init_node('bar_task_executive')
+    rospy.init_node('task_1_executive')
 
-    ## Instantiate the `ArmControl()` object
-    body_obj = MoveGroupClient()
-    head_obj = PointHeadClient()
-    body_obj.init_pose()
-    head_obj.set_pan_tilt(pan=0.45,duration=2)
-    head_obj.set_pan_tilt(pan=-0.45,duration=4)
-    head_obj.set_pan_tilt(duration=2)
+    obj = TaskExecutive()
+    
 
     rospy.spin()
-    # raw_input("Press enter to move Fetch to it's initial arm configuration")
-    # obj.init_pose()
-    # print("")
-    
-    # obj.relax_arm()
-    # raw_input("My arm is in relax mode. You can move it and hover it above the center top of the object you want me to disinfect. Once you have done that, press Enter.")
-    
-    # raw_input("I will start recording the cleaning task once you press enter")
-    # obj.record()
-    ## Notify user that they can move the arm
-    # rospy.loginfo("Relaxed arm node activated. You can now move the manipulator")
-    # print()
-
-    # obj.playback()
-    # rospy.loginfo("Type Ctrl + C when you are done recording")
-    # rospy.spin()
